ed2985fe617b50630415f41d0c407f9fe76b6ad9
OszvaldRenata <oszvaldreni@gmail.com>
2020-10-20 15:11:49 +0200

    1.Feladat: Kinézet elkészítése

---
+++ app/src/main/java/com/example/ko_papir_ollo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ko_papir_ollo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
